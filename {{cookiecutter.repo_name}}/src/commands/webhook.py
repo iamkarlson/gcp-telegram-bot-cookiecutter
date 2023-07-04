1b627024e0454a681d8a77bf929b95fd689c9689
George Green <iamkarlson@gmail.com>
2023-07-04 22:14:12 +0200

    added missing variable

--- {{cookiecutter.repo_name}}/src/commands/webhook.py
+++ {{cookiecutter.repo_name}}/src/commands/webhook.py
@@ -1,5 +1,12 @@
 from telegram import Message
 
+import os
+
+from telegram import Bot
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+bot = Bot(token=BOT_TOKEN)
+
 
 def command_webhook(message: Message):
     return bot.get_webhook_info().to_json()
